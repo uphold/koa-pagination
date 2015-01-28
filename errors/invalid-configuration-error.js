33d59c3360f7e9997519c90314892bb43b25656f
Francisco Cardoso <fvc@seegno.com>
2015-01-28 17:17:26 +0000

    Fix pagination object
    This commit also adds some missing validations and more tests with new
    use cases.

---
+++ errors/invalid-configuration-error.js
@@ -0,0 +1,12 @@
+
+/**
+ * Module dependencies.
+ */
+
+var errors = require('create-error');
+
+/**
+ * Export `InvalidConfigurationError`.
+ */
+
+module.exports = errors('InvalidConfigurationError');
--- test/koa-paginate_test.js
+++ test/koa-paginate_test.js
@@ -31,14 +31,45 @@ describe('paginate', function() {
   it('should accept a `maximum` option', function *() {
     var app = koa();
 
-    app.use(paginate({
-      maximum: 3
-    }));
+    app.use(paginate({ maximum: 3 }));
 
     yield request(app.listen())
       .get('/')
       .expect(206)
       .expect('Content-Range', 'bytes 0-2/*')
+      .end();
+  });
+
+  it('should give and error if `maximum` is not a number', function *() {
+    var app = koa();
+
+    app.use(paginate({ maximum: 'invalid' }));
+
+    yield request(app.listen())
+      .get('/')
+      .expect(500)
+      .end();
+  });
+
+  it('should give and error if `maximum` is 0', function *() {
+    var app = koa();
+
+    app.use(paginate({ maximum: 0 }));
+
+    yield request(app.listen())
+      .get('/')
+      .expect(500)
+      .end();
+  });
+
+  it('should give and error if `maximum` is lower than 0', function *() {
+    var app = koa();
+
+    app.use(paginate({ maximum: -1 }));
+
+    yield request(app.listen())
+      .get('/')
+      .expect(500)
       .end();
   });
 
@@ -79,22 +110,55 @@ describe('paginate', function() {
       .end();
   });
 
-  it('should not allow `limit` value superior to `maximum`', function *() {
+  it('should give and error if `first position` value is higher than `length`', function *() {
     var app = koa();
 
-    app.use(paginate({
-      maximum: 3
-    }));
+    app.use(paginate());
+
+    app.use(function *() {
+      this.pagination.length = 10;
+    });
 
     yield request(app.listen())
       .get('/')
-      .expect(206)
-      .set('Range', 'items=0-5')
-      .expect('Content-Range', 'items 0-2/*')
+      .set('Range', 'items=10-12')
+      .expect(416)
       .end();
   });
 
-  it('should not allow `limit` value superior to `length`', function *() {
+  it('should give and error if `first position` and `last position` have equal values and are equal to `length`', function *() {
+    var app = koa();
+
+    app.use(paginate());
+
+    app.use(function *() {
+      this.pagination.length = 10;
+    });
+
+    yield request(app.listen())
+      .get('/')
+      .set('Range', 'items=10-10')
+      .expect(416)
+      .end();
+  });
+
+  it('should give and error if `first position` and `last position` have equal values and are higher than `length`', function *() {
+    var app = koa();
+
+    app.use(paginate());
+
+    app.use(function *() {
+      this.pagination.length = 10;
+    });
+
+    yield request(app.listen())
+      .get('/')
+      .set('Range', 'items=11-11')
+      .expect(416)
+      .end();
+  });
+
+  it('should not allow `last position` value to be higher than `length`', function *() {
     var app = koa();
 
     app.use(paginate());
@@ -111,37 +175,85 @@ describe('paginate', function() {
       .end();
   });
 
-  it('should set `limit` to `N+1` when `Range` is `items=0-N`', function *() {
+  it('should not allow `last position` to be equal to `length`', function *() {
     var app = koa();
-    var n = 5;
 
     app.use(paginate());
 
     app.use(function *() {
-      this.pagination.limit.should.equal(n + 1);
+      this.pagination.length = 20;
     });
 
     yield request(app.listen())
       .get('/')
+      .set('Range', 'items=0-20')
       .expect(206)
-      .set('Range', util.format('items=0-%s', n))
+      .expect('Content-Range', 'items 0-19/20')
       .end();
   });
 
-  it('should set `offset` to `N` when `Range` is `items=N-5`', function *() {
+  it('should not allow `last position` value to be higher than `maximum`', function *() {
     var app = koa();
-    var n = 2;
+
+    app.use(paginate({ maximum: 3 }));
+
+    yield request(app.listen())
+      .get('/')
+      .expect(206)
+      .set('Range', 'items=0-5')
+      .expect('Content-Range', 'items 0-2/*')
+      .end();
+  });
+
+  it('should use the diference between `last position` and `first position`, plus one, as `limit`', function *() {
+    var app = koa();
+
+    var lastPosition = 6;
+    var firstPosition = 2;
 
     app.use(paginate());
 
     app.use(function *() {
-      this.pagination.offset.should.equal(n);
+      this.pagination.limit.should.equal(lastPosition - firstPosition + 1);
     });
 
     yield request(app.listen())
       .get('/')
       .expect(206)
-      .set('Range', util.format('items=%s-5', n))
+      .set('Range', util.format('items=%s-%s', firstPosition, lastPosition))
+      .end();
+  });
+
+  it('should use the `first position` as `offset`', function *() {
+    var app = koa();
+    var firstPosition = 2;
+
+    app.use(paginate());
+
+    app.use(function *() {
+      this.pagination.offset.should.equal(firstPosition);
+    });
+
+    yield request(app.listen())
+      .get('/')
+      .expect(206)
+      .set('Range', util.format('items=%s-5', firstPosition))
+      .end();
+  });
+
+  it('should set the `byte-range-spec` to `*` if length is 0', function *() {
+    var app = koa();
+
+    app.use(paginate());
+
+    app.use(function *() {
+      this.pagination.length = 0;
+    });
+
+    yield request(app.listen())
+      .get('/')
+      .expect(206)
+      .expect('Content-Range', 'bytes */0')
       .end();
   });
 });
